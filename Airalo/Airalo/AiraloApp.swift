6ebf4cd31736c30332226502a618fd7dde5e5659
petarmitevski <petar.m@slicelife.com>
2025-05-07 20:58:10 +0200

    Initial Xcode project created

---
+++ Airalo/Airalo/AiraloApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiraloApp.swift
+//  Airalo
+//
+//  Created by Petar Mitevski on 7.5.25.
+//
+
+import SwiftUI
+
+@main
+struct AiraloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
